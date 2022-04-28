96ccf891d3e5ffa01e85cd2af0fe8f5a8e15c137
Matt Marsico <mjmarsico@hotmail.com>
2022-04-27 20:35:51 -0500

    added cmdline support

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
+#!/usr/bin/env node
+
 import PracticeTest from "./classes.js";
+import yargs from "yargs"
+import chalk from "chalk"
+import inquirer from "inquirer"
 
-const firstTest = new PracticeTest("english", 5, 6, "5:35");
-
-firstTest.printSummary();
+inquirer
+    .prompt([
+        { type: 'list', message: "What would you like to do?", name: 'command', choices: [
+            'Print Scores',
+            'Print Average for Test',
+            'Print last test date'
+        ] }, { type: 'input', message: "What is your favorite color?", name: "favoriteColor"}
+    ])
+    .then(answers => {
+        console.log(answers)
+    });
 
