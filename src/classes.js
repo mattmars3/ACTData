8a6274b3d752be1a1dd20acb5f61f4a449ce413d
Matt Marsico <mjmarsico@hotmail.com>
2022-04-27 19:13:51 -0500

    refined code

--- src/classes.js
+++ src/classes.js
@@ -1,14 +1,6 @@
 //  This class represents a practice test and has all field 
 export default class PracticeTest {
-    constructor(theSubject, theDate, amntCorrect, totQuest, timeTaken, theNotes="") {
-        this.subject = theSubject;
-        this.practiceDate = theDate; // A Date object of when I did the practice
-        this.amountCorrect = amntCorrect;  
-        this.totalQuestions = totQuest;
-        this.timeToComplete = timeTaken; // The time (in seconds) to complete all of the section
-        this.theNotes = theNotes; // Optional Notes about the test
-    }
-    createPracticeTest(subject, amntCorrect, totalQuest, timeTaken, notes="", date="") {
+    constructor(subject, amntCorrect, totalQuest, timeTaken, notes="", date="") {
         // ensure that the amount of questions correct and total amount of questions are valid
         if (parseInt(totalQuest) == NaN || parseInt(amntCorrect) == NaN) {
             return false;
@@ -19,16 +11,38 @@ export default class PracticeTest {
         const todayDate = new Date();
         let parsedDate;
         if (date == "") {
-            parsedDate = `${todayDate.getFullYear}-${todayDate.getMonth}-${todayDate.getDate}`
+            parsedDate = `${todayDate.getFullYear()}-${todayDate.getMonth()}-${todayDate.getDate()}`
         } else { parsedDate = date; }
 
         // turn timeTaken into seconds
         const timeArr = timeTaken.split(":")
-        const timeSeconds = timeArr[0] * 60 + timeArr[1];
+        const timeSeconds = parseInt(timeArr[0]) * 60 + parseInt(timeArr[1]);
 
         // actually construct and return the practiceTest object
-        return new PracticeTest(
-            subject.toLowerCase(), parseInt(amntCorrect), parseInt(totalQuest), timeSeconds, parsedDate, notes
-        )
+        this.subject = subject.toLowerCase(); // subject name in lower case
+        this.amntCorrect = parseInt(amntCorrect); // amount correct
+        this.totalQuestions = parseInt(totalQuest); // total amount of questions
+        this.timeToComplete = timeSeconds; // time used to complete the questions in seconds
+        this.practiceDate = parsedDate; // date of the practice in order YYYY-MM-DD
+        this.notes = notes;
+    }
+
+    practiceTimeToString() {
+        let returnString = "";
+        return Math.floor(this.timeToComplete / 60).toString() + ":" + this.timeToComplete % 60;
+    }
+
+    static capitalize(string) {
+        return string.substring(0, 1).toUpperCase() + string.substring(1);
+    }
+
+    getSubject() {return this.subject;}
+
+    printSummary() {
+        console.log(PracticeTest.capitalize(this.subject) + " Practice Test on " + this.practiceDate + "\nScore: " + this.amntCorrect + "/" + this.totalQuestions)
+        console.log("Time: " + this.practiceTimeToString())
+        if (this.notes != "") {
+            console.log("Notes: " + notes)
+        }
     }
 }
